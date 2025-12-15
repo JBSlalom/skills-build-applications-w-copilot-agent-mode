20542606b0286897a534e46d239f033cc345ca01
JBSlalom <86677939+JBSlalom@users.noreply.github.com>
2025-12-15 14:23:21 +0000

    Update settings and URLs for Codespaces support

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,18 @@ SECRET_KEY = 'django-insecure-@@=oz8lze=k8^aboq#r7w_!n4445_bj90srk+=9kdc-94roo!!
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow Codespace URL and localhost
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    ALLOWED_HOSTS = [
+        f"{codespace_name}-8000.app.github.dev",
+        "localhost",
+        "127.0.0.1"
+    ]
+else:
+    ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet, api_root
+import os
+
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -24,6 +27,13 @@ router.register(r'teams', TeamViewSet, basename='team')
 router.register(r'activities', ActivityViewSet, basename='activity')
 router.register(r'workouts', WorkoutViewSet, basename='workout')
 router.register(r'leaderboards', LeaderboardViewSet, basename='leaderboard')
+
+# API endpoint base URL info for Codespaces
+# Use this format for API requests:
+#   https://$CODESPACE_NAME-8000.app.github.dev/api/[component]/
+# Example:
+#   https://$CODESPACE_NAME-8000.app.github.dev/api/activities/
+# $CODESPACE_NAME is set automatically in Codespaces
 
 urlpatterns = [
     path('admin/', admin.site.urls),
